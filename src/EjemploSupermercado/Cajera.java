dd955a88773f39d2fea981b2ee52c1a541ab9827
david81094 <david.camachoencina@iesvalleinclan.es>
2019-10-02 10:31:19 +0200

    Update

---
+++ src/EjemploSupermercado/Cajera.java
@@ -0,0 +1,31 @@
+package EjemploSupermercado;
+
+public class Cajera {
+
+    private String nombre;
+
+    public Cajera(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void procesarCompra(Cliente cliente, long timeStamp) {
+        System.out.println("La cajera " + this.nombre + " comienza a procesar la compra del cliente " + cliente.getNombre() + "en el tiempo" + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+
+        for (int i = 0; i < cliente.getCarroCompra().length; i++) {
+            this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            System.out.println("Procesado el producto " + (i + 1) + "tiempo:" + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+        }
+
+        System.out.println("La cajera: " + this.nombre + "ha terminado de procesar" + cliente.getNombre() + "en el tiempo" + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+
+    }
+
+    private void esperarXsegundos(int segundos) {
+        try {
+            Thread.sleep(segundos * 1000);
+        }catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}

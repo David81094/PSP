abb77d7f0ca1e3f4945ef8826c045b5caf568dff
david81094 <david.camachoencina@iesvalleinclan.es>
2019-10-02 10:33:27 +0200

    Update

--- src/EjemploSupermercado/Main.java
+++ src/EjemploSupermercado/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente("Cliente 1", new int[] {2,2,1,5,2,3});
+        Cliente cliente1 = new Cliente("Cliente 1", new int[] {2,2,1,5,2,3});
         Cliente cliente2= new Cliente("Cliente 2", new int[] {1,3,5,1,1});
 
         Cajera cajera1 = new Cajera("Cajera 1");
@@ -12,7 +12,7 @@ public class Main {
 
         long initialTime = System.currentTimeMillis();
 
-        cajera1.procesarCompra(cliente, initialTime);
+        cajera1.procesarCompra(cliente1, initialTime);
         cajera2.procesarCompra(cliente2, initialTime);
 
     }

58a8d5abf867e2ec71fc29b19ab085a970bf869d
david81094 <david.camachoencina@iesvalleinclan.es>
2019-10-02 10:34:53 +0200

    Update

--- src/EjemploSupermercado/Cajera.java
+++ src/EjemploSupermercado/Cajera.java
@@ -9,14 +9,14 @@ public class Cajera {
     }
 
     public void procesarCompra(Cliente cliente, long timeStamp) {
-        System.out.println("La cajera " + this.nombre + " comienza a procesar la compra del cliente: " + cliente.getNombre() + " en el tiempo " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+        System.out.println("La cajera " + this.nombre + " comienza a procesar la compra del cliente: " + cliente.getNombre() + ", en el tiempo " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 
         for (int i = 0; i < cliente.getCarroCompra().length; i++) {
             this.esperarXsegundos(cliente.getCarroCompra()[i]);
-            System.out.println("Procesado el producto: " + (i + 1) + " tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+            System.out.println("Procesado el producto: " + (i + 1) + ", tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
         }
 
-        System.out.println("La cajera: " + this.nombre + "ha terminado de procesar: " + cliente.getNombre() + " en el tiempo " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+        System.out.println("La cajera: " + this.nombre + " ha terminado de procesar, " + cliente.getNombre() + " en el tiempo " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 
     }
 
